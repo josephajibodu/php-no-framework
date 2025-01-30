4c31ad6fdac698c14939434240398868110f9d01
josephajibodu <josephajibodu@gmail.com>
2025-01-31 00:52:49 +0100

    added error handling capabilites with whoops
    this gives us a pretty error interface, however, errors should only
    be displayed in development server, never in production. you know why

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # PHP without Framework: The right way
 
+[no-framework-tutorial](https://git.php.fail/lubiana/no-framework-tutorial)
+
 ## Things the user must know
 
 Declare Strict Types in PHP
--- src/Bootstrap.php
+++ src/Bootstrap.php
@@ -2,4 +2,24 @@
 
 namespace JosephAjibodu\PhpNoFramework;
 
-echo 'Hello World!';
+use Whoops\Handler\PrettyPageHandler;
+use Whoops\Run;
+
+require __DIR__ . '/../vendor/autoload.php';
+
+$environment = getenv('ENVIRONMENT') ?: 'dev';
+
+error_reporting(E_ALL);
+
+$whoops = new Run;
+if ($environment === 'dev') {
+    $whoops->pushHandler(new PrettyPageHandler);
+} else {
+    $whoops->pushHandler(function (\Throwable $e) {
+        error_log("Error: ". $e->getMessage(), $e->getCode());
+        echo 'Friendly error page and send an email to the developer';
+    });
+}
+$whoops->register();
+
+throw new \Exception('Test exception');
